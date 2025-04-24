92395918e24422a02c6d4cdfc0109b09520ff1a2
Itrkine Abdellah <48138349+Abdellah-Itrkine@users.noreply.github.com>
2025-04-24 22:49:19 +0100

    delete old file and use java spring boot project

---
+++ hisabAlBaraka/src/main/java/me/abdellah/hisabalbaraka/HisabAlBarakaApplication.java
@@ -0,0 +1,13 @@
+package me.abdellah.hisabalbaraka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisabAlBarakaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HisabAlBarakaApplication.class, args);
+    }
+
+}
---
+++ hisabAlBaraka/src/test/java/me/abdellah/hisabalbaraka/HisabAlBarakaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdellah.hisabalbaraka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisabAlBarakaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
